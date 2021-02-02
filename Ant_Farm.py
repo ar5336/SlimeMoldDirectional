1a91b1bf28e930a933bf90a9d0ea22d94d3f80c6
Artem Romanenko <46803624+ar5336@users.noreply.github.com>
2021-02-02 15:50:35 -0500

    Add files via upload
    Fixed color display. press the 'c' key to toggle from greyscale.
    
    the evaluation function block is full of commented out evaluation functions, i'll clean the whole thing up eventually.
    I'm trying to figure out how to force it to make pretty pictures. so far it's doing an ok job at best.

--- Ant_Farm.py
+++ Ant_Farm.py
@@ -18,12 +18,14 @@ def line(p1, p2, lr_canvas, ud_canvas, value):
         return
 
     for i in range(int(dist)):
-        px = x1 + dx * i / dist
-        py = y1 + dy * i / dist
+        px = x1 - dx * (i / dist)
+        py = y1 - dy * (i / dist)
 
         if not is_oob(lr_canvas, px, py):
             lr_canvas[int(px), int(py)] += value * math.cos(dir)
+            lr_canvas[int(px), int(py)] = max(-100, min(100, lr_canvas[int(px), int(py)]))
             ud_canvas[int(px), int(py)] += value * -math.sin(dir)
+            ud_canvas[int(px), int(py)] = max(-100, min(100, ud_canvas[int(px), int(py)]))
 
 
 def is_oob(canvas, x, y):
@@ -38,7 +40,7 @@ class Ant:
         self.px = px
         self.py = py
         self.dir = random.uniform(0, 2*math.pi)
-        self.dist = 5
+        self.dist = 10
         self.scan_angle = math.pi/4
         self.num_scans = 5
 
@@ -63,14 +65,29 @@ class Ant:
 
             scan_results.append((direction, pheromone_direction, pheromone_intensity))
 
+        random.shuffle(scan_results)
+
         # find the best direction to walk in
         best_direction = 0
         best_eval = -1
         for dir, pdir, pint in scan_results:
-            eval = pint + 250 - abs(pdir - dir)
+            eval = pint + 500 - abs(pdir - dir)
+            # eval = min(20 - abs(20 - pint), 15)
+            # eval = 15 - abs(pdir - dir) * (pint / 50)
+            # eval = 2*math.pi - abs(pdir - dir) + pint/500
+            # eval = 0
+            # eval = pint/10
+            # if abs(dir - pdir) < math.pi/8 or abs(abs(dir - pdir) - math.pi/2) < math.pi/8:
+            #     eval = pint
             if eval > best_eval:
                 best_eval = eval
                 best_direction = dir
+
+        if best_eval > 10:
+            best_direction += math.pi * 2
+
+        self.dir = best_direction
+
         self.walk(lr_canvas, ud_canvas, best_direction)
 
     def pos_from_dir(self, direction):
@@ -85,33 +102,27 @@ class Ant:
         dx = math.cos(direction)*self.dist
         dy = -math.sin(direction)*self.dist
 
-        # target_x = int(self.px + dx)
-        # target_y = int(self.py + dy)
-        #
-        # if target_x >= w or target_x < 0:
-        #     dx *= -1
-        # if target_y >= h or target_y < 0:
-        #     dy *= -1
-
         new_px = (self.px + dx) % w
         new_py = (self.py + dy) % h
 
-        line((self.px, self.py), (new_px, new_py), lr_canvas, ud_canvas, 1)
+        line((self.px, self.py), (new_px, new_py), lr_canvas, ud_canvas, 3)
 
         self.px = new_px
         self.py = new_py
 
 
 def main():
+    GREYSCALE = True
+
     # WDITH = 500
     # HEIGHT = 500
-    lr_canvas = np.zeros((500, 500))
-    ud_canvas = np.zeros((500, 500))
+    lr_canvas = np.zeros((500, 500), dtype=float)
+    ud_canvas = np.zeros((500, 500), dtype=float)
 
     w, h = lr_canvas.shape
 
     ants = []
-    for i in range(3000):
+    for i in range(500):
         ants.append(Ant(random.randrange(0, w-1), random.randrange(0, h-1)))
 
     while True:
@@ -121,21 +132,26 @@ def main():
         lr_canvas = lr_canvas * .99
         ud_canvas = ud_canvas * .99
 
-        lr_canvas = cv2.GaussianBlur(lr_canvas, (3, 3), .5)
-        ud_canvas = cv2.GaussianBlur(ud_canvas, (3, 3), .5)
+        lr_canvas = cv2.GaussianBlur(lr_canvas, (3, 3), .6)
+        ud_canvas = cv2.GaussianBlur(ud_canvas, (3, 3), .6)
 
-        hue = np.arctan2(lr_canvas, ud_canvas) * 255
-        saturation = np.zeros((500, 500))
-        saturation.fill(255)
-        value = np.sqrt(np.square(lr_canvas) + np.square(ud_canvas))
-        hsv_im = np.dstack((hue, saturation, value))
-        hsv_im = np.float32(hsv_im)
+        value = np.sqrt(np.square(lr_canvas) + np.square(ud_canvas))/5
+        if not GREYSCALE:
+            hue = (np.arctan2(lr_canvas, ud_canvas, dtype=float) + math.pi) * 255/(2*math.pi)
+            saturation = np.zeros((500, 500), dtype=float)
+            saturation.fill(.5)
+            hsv_im = np.dstack((hue, saturation, value))
+            hsv_im = np.float32(hsv_im)
 
-        # cv2.imshow("ant farm", hsv_im)
+            # cv2.imshow("ant farm", hsv_im)
 
-        bgr_im = cv2.cvtColor(hsv_im, cv2.COLOR_HSV2BGR)
-        cv2.imshow("ant farm", bgr_im)
+            bgr_im = cv2.cvtColor(hsv_im, cv2.COLOR_HSV2RGB)
+            cv2.imshow("ant farm", bgr_im)
+        else:
+            cv2.imshow("ant farm", value)
         key = cv2.waitKey(1)
+        if key == ord('c'):
+            GREYSCALE = not GREYSCALE
         if key == ord('q'):
             break
 
